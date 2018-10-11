066f86b8da9ce78e1efe2697d55db3bea5e01fc4
Suja Chinnaswamy <suja@netwealth.com.au>
2018-10-11 11:54:05 +1100

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a name here:");
+            string name = Console.ReadLine() ;
+            Console.WriteLine("Hello " + name +"!");
+        }
+    }
+}
