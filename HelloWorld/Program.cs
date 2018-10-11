d54a190028d6a9613abfc8b4984c520fa45165c9
Suja Chinnaswamy <suja@netwealth.com.au>
2018-10-11 15:24:24 +1100

    Refactor

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,12 +4,11 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
+        static void Main()
+        {            
             Console.WriteLine("Please enter a name here:");
-            string name = Console.ReadLine() ;
-            Console.WriteLine($"Hello " {name} "!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
         }
     }
 }
