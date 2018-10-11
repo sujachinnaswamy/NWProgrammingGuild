dc3796241fe58f3564ce3bdcd1ec77faa227d565
Suja Chinnaswamy <suja@netwealth.com.au>
2018-10-11 12:05:24 +1100

    Added comment

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Test
             Console.WriteLine("Please enter a name here:");
             string name = Console.ReadLine() ;
             Console.WriteLine("Hello " + name +"!");
