05bcdd2a44d1c3164ae5fc176c409b9018d65a81
Suja Chinnaswamy <suja@netwealth.com.au>
2018-10-26 14:57:56 +1100

    Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main()
+        {            
+            for (int i = 0; i< 5; i++)
+            {
+               Console.WriteLine(new string('*', (i < 5) ? i:5));
+            }
+        }
+    }
+}
