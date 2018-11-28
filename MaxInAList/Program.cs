24c5494fb32c48290c06c88e08829119c76775ca
Suja Chinnaswamy <suja@netwealth.com.au>
2018-11-28 14:12:44 +1100

    New exercise max in a list

---
+++ MaxInAList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main()
+        {            
+            int[] arrayofNumbers= new int[100];
+            int i, arraysize;
+
+            Console.Write("Input the number of elements to be stored in the array :");
+	        arraysize= Convert.ToInt32(Console.ReadLine());  
+   	
+            Console.Write("Input {0} elements in the array :\n",arraysize);
+            for(i=0;i<arraysize;i++)
+            {
+	          Console.Write("element {0} : ",i);
+		      arrayofNumbers[i] = Convert.ToInt32(Console.ReadLine());  
+	        }
+            int max = arrayofNumbers.Max();
+
+            Console.WriteLine($"Largest number is {max}");
+
+        }
+    }
+}
